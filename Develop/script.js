655a553157e4c025657e67c3e509c60b643cb0d9
Rajni bala <balatamna16@gmail.com>
2023-05-24 18:10:34 -0400

    use dayjs to display day and date

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//Add code to display the current date in the header of the page.
+var today = dayjs();
+$('#currentDay').text(today.format('MMM D, YYYY'));
+//Add code to display the current day .
+var dayWeek = today.format('dddd');
+$('#currentDayoftheWeek').text(dayWeek);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
